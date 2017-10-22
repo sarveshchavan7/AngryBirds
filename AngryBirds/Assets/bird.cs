59f2e705af067435bb19bd65025b7861e3e5fb3a
care4u <sarveshchavan777@gmail.com>
2017-10-22 12:57:34 -0500

    First commit
    First commit

---
+++ AngryBirds/Assets/bird.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class bird : MonoBehaviour {
+
+    private bool isPressed = false;
+    public Rigidbody2D rb;
+    public float releaseTime = .15f;
+    public float maxDragDistance = 3f;
+    public Rigidbody2D hook;
+    public GameObject nextBird;
+
+    // Update is called once per frame
+    void Update () {
+        if (isPressed) {
+            Vector2 mousePos= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, hook.position) > maxDragDistance)
+           {
+                rb.position = hook.position + (mousePos - hook.position).normalized * maxDragDistance;
+            }
+           else {
+                rb.position = mousePos;
+           }
+        }
+         
+	}
+
+     void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+     void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release() {
+       yield return new WaitForSeconds(releaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+
+        yield return new WaitForSeconds(2f);
+
+        if (nextBird != null)
+        {
+            nextBird.SetActive(true);
+        }
+        else {
+            enemy.enemyAlive = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        
+    }
+}
---
+++ AngryBirds/Assets/enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class enemy : MonoBehaviour {
+
+    public float health = 3f;
+    public GameObject deathEffect;
+   public static int enemyAlive = 0;
+
+    private void Start()
+    {
+        enemyAlive++;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.magnitude>health) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Instantiate(deathEffect, transform.position, Quaternion.identity);
+        enemyAlive--;
+        if (enemyAlive <= 0) {
+          
+            Debug.Log("WON");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    
+        Destroy(gameObject);
+    }
+}
